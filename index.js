7e985087fb27ebaf0d4153617fadb15ee0d675df
Amber <martinez.amber1028@gmail.com>
2021-12-15 09:58:02 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution here
+const driversExt = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function findMatching(group, name) {
+    const result = group.filter(word => word.toUpperCase() === name.toUpperCase())
+    return result
+}
+
+function fuzzyMatch(group, letters) {
+    const result = group.filter(letter => letter.substring(0,2) === letters.substring(0,2))
+    return result
+}
+
+
+
+function matchName(group, name) {
+    return group.filter(nameMatch => nameMatch.name === name)
+}
